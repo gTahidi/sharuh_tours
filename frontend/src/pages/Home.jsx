145d0e8f1d5b68aa3874a3ddc8411bba008eb9bf
cjephuneh <calebjephuneh@gmail.com>
2023-12-30 22:57:23 +0300

    updates

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../styles/home.css'
 import { Container, Row, Col, CardSubtitle } from 'reactstrap'
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
+import heroImg from '../assets/images/WhatsApp Image 2023-12-01 at 12.14.34 PM (1).jpeg'
+import heroImg02 from '../assets/images/WhatsApp Image 2023-12-01 at 12.14.32 PM (1).jpeg'
+import heroVideo from '../assets/images/WhatsApp Image 2023-12-01 at 12.14.34 PM (1).jpeg'
 import worldImg from '../assets/images/world.png'
 import experienceImg from '../assets/images/experience.png'
 
